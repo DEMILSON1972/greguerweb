4a3407bf8476e526e346e49c3e29d48888603e95
unknown <demigreguer@gmail.com>
2020-04-17 22:06:37 -0300

    primeiro comite hub

---
+++ cabecalho.php
@@ -0,0 +1,54 @@
+<div id = "cabecalho_superior">
+	<nav id="menu_cima">                          
+			<ul>
+				<li> <a href= "#" > MINHA CONTA </a>  </li>
+				<li> <a href= "#" > MEU CARRINHO </a>  </li>
+				<li> <a href= "#" > LOGAR </a>  </li>
+				<li> <a href= "#" > CADASTRAR </a>  </li>
+
+			</ul>
+
+
+
+	</nav>
+</div>	
+
+<div id = "cabecalho_meio">
+  <h1> DEMILSON GREGUER </h1>
+  <p> NENHUM ITEM NO SEU CARRINHO DE COMPRAS </p>
+  	<section>
+  		<form action="">
+  			<label>
+  				<span>  buscar </span>
+  					<input type="search" name="pesquisa" id ="pesquisa">
+  			</label>
+
+  		</form>
+
+  	</section>                   
+
+<div>
+	
+<div = "cabecalho_inferior">
+		<nav id="menu_principal">  
+			<ul>
+				<li> <a href="#"> Nome </a>    </li>
+				<li> <a href="#"> Produtos </a>    </li>
+				<li> <a href="#"> Fale Conosco </a>    </li>
+				<li> <a href="#"> Quem Somos </a>    </li>
+
+
+			</ul>
+
+	   </nav>
+
+
+</div>
+
+
+
+</div>	
+
+<div id = "cabecalho_inferior">
+
+</div>	
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>LOJA VIRTUAL DG</title>
+	<link href="/css.css" rel="stylesheet"  type="text/css" /> 
+</head>
+<body>
+              
+       <div id = "Principal">
+       		<section id="cabecalho">
+       	     
+              <?php include_once ("cabecalho.php"); ?>
+                    
+              	</section>
+       		
+       		<section id="corpo"> <!-- inicio div corpo -->
+       			
+       				
+       		</section> <!-- fim div corpo -->
+
+       		<footer id="rodape"> <!-- inicio div rodape -->
+       			
+
+       		</footer> <!-- fim div corpo -->
+
+
+       </div> <!-- fim div principal --->
+ </body>
+</html>
+
+
